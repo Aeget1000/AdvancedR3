0af9a16a3e806ff8bbf715678a13be5d2a871e15
Christian A. Andersen <christianarnfeldt@gmail.com>
2025-12-03 10:03:50 +0100

    Added the function: create_table_descriptive_stats

--- R/functions.R
+++ R/functions.R
@@ -1,6 +1,21 @@
 # super clean and tested functions live here
 
 
+#' A function that makes a beautiful with Metabolite - Mean SD
+#'
+#' @param data
+#'
+#' @returns A data.frame/tibble.
+create_table_descriptive_stats <-
+  function(data) {
+    data |>
+      dplyr::group_by(metabolite) |>
+      dplyr::summarise(across(value, list(mean = mean, sd = sd))) |>
+      dplyr::mutate(across(where(is.numeric), function(x) formatC(x, digits = 2, format = "f"))) |>
+      dplyr::mutate(MeanSD = glue::glue("{value_mean}  ({value_sd})")) |>
+      dplyr::select(Metabolite = metabolite, `Mean SD` = MeanSD)
+  }
+
 
 #
 #  # My stupid function that multipliers two numbers
